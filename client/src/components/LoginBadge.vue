8fb0335db5089ab5649460e950b47c105c31643d
Cruzj39 <cruzj39@newpaltz.edu>
2023-03-17 14:57:19 -0400

    Added Navbar

--- client/src/components/LoginBadge.vue
+++ client/src/components/LoginBadge.vue
@@ -6,114 +6,48 @@ const session = useSession();
 const logout = useLogout();
 
 function logout2() {
-    logout();
+  logout();
 }
 const isdropActive = ref(false);
 
-const users = ref( getUsers());
+const users = ref(getUsers());
 
 function toggledrop() {
-        isdropActive.value = !isdropActive.value;
-    }
+  isdropActive.value = !isdropActive.value;
+}
 
 </script>
 
 <template>
-    
-        
-
-    <div class="navbar-item has-dropdown is-hoverable">
-              <div class="navbar-dropdown">
-                <a class="navbar-item" href="https://bulma.io/documentation/overview/start/">
-                  Overview
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/overview/modifiers/">
-                  Modifiers
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
-                  Columns
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
-                  Layout
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
-                  Form
-                </a>
-                <hr class="navbar-divider">
-                <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
-                  Elements
-                </a>
-                <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
-                  Components
-                </a>
-              </div>
-            </div>
-            <div class="navbar-item has-dropdown is-hoverable" v-if="session.user">
-                Welcome, {{ session.user.name }}
-                (<a @click="logout2()">logout</a>)
-            </div>
-            <div class="navbar-item" v-else>
-                <a class="button is-info">
-                    <span class="icon">
-                        <i class="fas fa-user-plus"></i>
-                    </span>
-                    <strong>Sign up</strong>
-                </a>
-                
-                <div :class="{ 'dropdown is-active': isdropActive }" @click="toggledrop" >
-  <div class="dropdown-trigger">
-    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu2">
-      <span>Login</span>
-      <span class="icon is-small">
-        <i class="fas fa-angle-down" aria-hidden="true"></i>
-      </span>
-    </button>
+  <div class="navbar-item has-dropdown is-hoverable" v-if="session.user">
+    <div class="navbar-item">Welcome, {{ session.user.name }}
+    <a class="button is-primary" @click="logout2()"><strong>Log out</strong></a></div>
   </div>
-  <div class="dropdown-menu" id="dropdown-menu2" role="menu">
-    <div class="dropdown-content" v-for=" user in users">
-        <div class="dropdown-item">
-            <p>{{ user.name }}</p>
-            <p>{{ user.email }}</p>
-            <button @click="login(user)">+</button>
+  <div class="navbar-item" v-else>
+    <a class="button is-primary">
+      <strong>Sign up</strong>
+    </a>
+
+    <div :class="{ 'dropdown is-active': isdropActive }" @click="toggledrop">
+      <div class="dropdown-trigger">
+        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu2">
+          <span>Log in</span>
+          <span class="icon is-small">
+            <i class="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </button>
+      </div>
+      <div class="dropdown-menu" id="dropdown-menu2" role="menu">
+        <div class="dropdown-content" v-for=" user in users">
+          <div class="dropdown-item">
+            <p @click=login(user)>{{ user.name }}</p>
+          </div>
+
         </div>
-      
+      </div>
     </div>
+
   </div>
-</div>
-
-                
-                <div class="navbar-item has-dropdown is-hoverable">
-              <div class="navbar-dropdown"> sda
-                <a class="navbar-item" href="https://bulma.io/documentation/overview/start/">
-                  Overview
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/overview/modifiers/">
-                  Modifiers
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
-                  Columns
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
-                  Layout
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
-                  Form
-                </a>
-                <hr class="navbar-divider">
-                <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
-                  Elements
-                </a>
-                <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
-                  Components
-                </a>
-              </div>
-            </div>
-            </div>
-
-
-
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -6,72 +6,108 @@ import Cart from './Cart.vue';
 import Flyout from './Flyout.vue';
 import LoginBadge from './LoginBadge.vue';
 
-    const isMenuActive = ref(false);
-    const isCartActive = ref(false);
+const isMenuActive = ref(false);
+const isCartActive = ref(false);
+const isdropActive = ref(false);
 
-    function toggleMenu() {
-        isMenuActive.value = !isMenuActive.value;
-        console.log({ isMenuActive });
-    }
+function toggleMenu() {
+  isMenuActive.value = !isMenuActive.value;
+  console.log({ isMenuActive });
+}
+
+function toggledrop() {
+  isdropActive.value = !isdropActive.value;
+}
 </script>
 
 <template>
-    <nav class="navbar is-primary">
-        <div class="container">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="https://bulma.io">
-            
-            <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="22" height="22" />
-            
-          </a>
-          <div class="navbar-burger" :class="{ 'is-active': isMenuActive }" @click="toggleMenu" >
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
+  <nav class="navbar is-primary">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="28" height="22" />
+        </a>
+          <RouterLink to="/" class="navbar-item">
+            <span class="icon">
+              <i class="fas fa-running"></i>
+            </span>
+            <span>My Activity</span>
+          </RouterLink>
+          <RouterLink to="/about" class="navbar-item">
+            <span class="icon">
+              <i class="fas fa-chart-line "></i>
+            </span>
+            <span>Statistics</span>
+          </RouterLink>
+          <RouterLink to="/products" class="navbar-item">
+            <span class="icon">
+              <i class="fas fa-people-group"></i>
+            </span>
+            <span>Friends Activity</span>
+          </RouterLink>
+        <div class="navbar-burger" :class="{ 'is-active': isMenuActive }" @click="toggleMenu">
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
+      </div>
+
+      <div class="navbar-menu" :class="{ 'is-active': isMenuActive }">
+        <div class="navbar-start">
+          <RouterLink to="/products" class="navbar-item">
+            <span class="icon">
+              <i class="fas fa-search"></i>
+            </span>
+            <span>People Search</span>
+          </RouterLink>
+          <div class="navbar-item">
+          <div class="dropdown is-hoverable">
+  <div class="dropdown-trigger">
+    <button class="button is-primary navbar-link" aria-haspopup="true" aria-controls="dropdown-menu4">
+      <span>Admin</span>
+    </button>
+  </div>
+  <div class="dropdown-menu" id="dropdown-menu4" role="menu">
+    <div class="dropdown-content">
+        <RouterLink to="/products" class="navbar-item">
+            Users
+          </RouterLink>
       
-        <div class="navbar-menu" :class="{ 'is-active': isMenuActive }">
-          <div class="navbar-start">
-
-            <RouterLink to="/" class="navbar-item">Home</RouterLink>
-            <RouterLink to="/about" class="navbar-item">About</RouterLink>
-            <RouterLink to="/products" class="navbar-item">Products</RouterLink>
-
-
-            
-          </div>
-      
-          <div class="navbar-end">
-
-            <LoginBadge />
-
-            <div class="navbar-item">
-              <div class="field is-grouped">
-                <p class="control">
-                  <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="https://bulma.io" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
-                    <span class="icon">
-                      <i class="fab fa-twitter"></i>
-                    </span>
-                    <span>
-                      Tweet
-                    </span>
-                  </a>
-                </p>
-              </div>
+    </div>
+  </div>
+</div>
+</div>
+        </div>
+        <div class="navbar-end">
+          <LoginBadge />
+          <div class="navbar-item">
+            <div class="field is-grouped">
+              <p class="control">
+                <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet"
+                  data-social-target="https://bulma.io" target="_blank"
+                  href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
+                  <span class="icon">
+                    <i class="fab fa-twitter"></i>
+                  </span>
+                  <span>
+                    Tweet
+                  </span>
+                </a>
+              </p>
             </div>
           </div>
         </div>
+      </div>
     </div>
-      </nav>
+  </nav>
 </template>
 
 
 <style scoped>
-    .quantity-tag {
-        position: absolute;
-        top: -5px;
-        right: -5px;
-        border-radius: 1rem;
-    }
+.quantity-tag {
+  position: absolute;
+  top: -5px;
+  right: -5px;
+  border-radius: 1rem;
+}
 </style>
