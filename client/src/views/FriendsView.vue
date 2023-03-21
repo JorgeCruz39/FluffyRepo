dc9e21f1e301240fc5540fa6edcc84a9f98e601d
JorgeCruz39 <98356857+JorgeCruz39@users.noreply.github.com>
2023-03-20 22:06:20 -0400

    Able to Add Workouts

--- client/src/views/FriendsView.vue
+++ client/src/views/FriendsView.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { useSession } from '@/model/session';
 import { ref } from 'vue';
-import { getWorkout } from '@/model/workout';
+import { addToWorkout, getWorkout, useWorkout, type Workout } from '@/model/workout';
 
 
 
 const workout = getWorkout();
 const session = useSession();
+const workouts1 = useWorkout();
+const newWorkout : Workout = ({} as any) as Workout;
+var date = new Date();
 function toggleForm() {
   isFormActive.value = !isFormActive.value;
 }
@@ -22,11 +25,11 @@ const isFormActive = ref(false);
 <template>
     
         <h1><strong>My Activity</strong></h1>
-        <div class="columns ">
+        <div class="columns " v-if="session.user">
             <div class="column is-half is-offset-one-quarter media">
                 <button class="button is-info is-fullwidth"  @click="toggleForm">Add Workout</button>
                 <br>
-                <form class="modal " :class="{ 'is-active': isFormActive }" >
+                <form @submit.prevent="addToWorkout(newWorkout,date,session.user?.username,)" class="modal " :class="{ 'is-active': isFormActive }" >
                     <div class="modal-background"></div>
                     <div class="modal-card">
                         <header class="modal-card-head"> 
@@ -34,33 +37,27 @@ const isFormActive = ref(false);
                         </header>
                         <section class="modal-card-body">
                             <div class="field">
-                                <label class="label"><Title></Title></label>
+                                <label class="label">Date</label>
                                 <div class="control">
-                                    <input class="input" type="Text" placeholder="Date" >
+                                    <input v-model="date" class="input" type="Date" placeholder="Date" >
                                 </div>
                             </div>
                             <div class="field">
                                 <label class="label">Distance</label>
                                 <div class="control">
-                                    <input class="input" type="text" placeholder="Distance" >
+                                    <input v-model="newWorkout.workoutDistance" class="input" type="text" placeholder="Distance" >
                                 </div>
                             </div>
                             <div class="field">
                                 <label class="label">Duration</label>
                                 <div class="control">
-                                    <input class="input" type="text" placeholder="Duration" >
-                                </div>
-                            </div>
-                            <div class="field">
-                                <label class="label">Pace</label>
-                                <div class="control">
-                                    <input class="input" type="text" placeholder="Pace" >
+                                    <input v-model="newWorkout.workoutDuration" class="input" type="text" placeholder="Duration" >
                                 </div>
                             </div>
                             <div class="field">
                                 <label class="label">type</label>
                                 <div class="select is-full-width">
-                                    <select>
+                                    <select v-model="newWorkout.workoutType" >
                                         <option value="Run">Run</option>
                                         <option value="Bike">Bike</option>
                                         <option value="Swim">Swim</option>
@@ -70,45 +67,54 @@ const isFormActive = ref(false);
                                 
                         </section>
                         <footer class="modal-card-foot">
-                            <RouterLink to="/MyActivity" class="button is-success"  @click="toggleForm" >Save changes</RouterLink>
+                            <button class="button is-success" type="submit" @click="toggleForm" >Save changes</button>
                             <RouterLink to="/MyActivity" class="button"  @click="toggleForm">Cancel</RouterLink>
                         </footer>
                     </div>
 
                 </form>
-                <div v-for="workouts in workout" >
-                <div>
-                    <article class="media box"  >
+
+                <div v-for="workoutss1 in workouts1.slice().reverse() ">
+                    <article class="media box" div v-if="session.user?.username == workoutss1.username">
                         <figure class="media-left">
                             <p class="image is-64x64">
-                                <img src="https://bulma.io/images/placeholders/128x128.png">
+                                <img :src="session.user.photo">
                             </p>
                         </figure>
                         <div class="media-content">
                             <div class="content">
                                 <p>
-                                    <strong class="space"> {{ workouts.username }} </strong>
-                                     <small class="space"> {{  workouts.username }}   </small>
-                                     <small class="space"> {{ workouts.workoutDate }}</small>
+                                    <strong class="space"> {{ workoutss1.username }} </strong>
+                                     <small class="space"> {{  workoutss1.username }}   </small>
+                                     <small class="space"> {{ workoutss1.workoutDate }}</small>
                                      <br>
-                                      <small>31m</small>
+                                     <p class="down"> <strong> Excecise done : {{ workoutss1.workoutType }}</strong></p>
                                       <div class="columns">
                                         <div class="column has-text-centered" style="display: flex; justify-content: space-around; align-items: center;">
                                             <div>
-                                                <h1 class=" has-text has-text-weight-bold" style="margin: 0px;"> {{ workouts.workoutDistance }}</h1>
+                                                <h1 class=" has-text has-text-weight-bold" style="margin: 0px;"> {{ workoutss1.workoutDistance }}</h1>
                                                 <caption class="caption is-half">Distance</caption>
                                             </div>
                                             <div>
-                                                <h1 class=" has-text has-text-weight-bold" style="margin: 0px;"> {{ workouts.workoutDuration }}</h1>
+                                                <h1 class=" has-text has-text-weight-bold" style="margin: 0px;"> {{ workoutss1.workoutDuration }}</h1>
                                                 <caption class="caption">Duration</caption>
                                             </div>
                                         </div>
-                                        <div class="column">
-                                            <img src="https://bulma.io/images/placeholders/128x128.png" style=" height: 100%;">
+                                        <div class="column" v-if="workoutss1.workoutType== 'Run' " >
+                                            <img src="https://hips.hearstapps.com/hmg-prod/images/running-is-one-of-the-best-ways-to-stay-fit-royalty-free-image-1036780592-1553033495.jpg?crop=0.668xw:1.00xh;0.260xw,0&resize=1200:*" style=" height: 100%;">
 
                                         </div>
+                                        <div class="column" v-else-if="workoutss1.workoutType== 'Bike' " >
+                                            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/60f32f8188683.560b8c8ec2614.jpg" style=" height: 100%;">
 
                                       </div>
+                                        <div class="column" v-else-if="workoutss1.workoutType== 'Swim' " >
+                                            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Amphibious%2C_Recon_Marines_prepare_for_Dive_School_170307-M-ZL982-128.jpg" style=" height: 100%;">
+                                        </div>
+                                        <div class="column" v-else>
+                                            <img src="https://bulma.io/images/placeholders/128x128.png" style=" height: 100%;">
+                                        </div>
+                                    </div>
                                 </p>
                             </div>
                             <nav class="level is-mobile">
@@ -127,11 +133,15 @@ const isFormActive = ref(false);
 
                         </div>
                     </article>
-                </div>
-                </div>
-                <div>
+            </div>
+        
+                
 
-                </div>
+
+
+
+                
+            
 
             </div>
             </div>
@@ -152,7 +162,7 @@ const isFormActive = ref(false);
     padding-left: 50px;
 }
 .space {
-              content: " ";
+              padding: 4px;
               white-space: pre;
           }
 
