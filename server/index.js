cd71b288eef976693b38c6c9e7f365a57f221036
JorgeCruz39 <98356857+JorgeCruz39@users.noreply.github.com>
2023-05-02 20:53:59 -0400

    IDK why gitignore is doing this

--- server/index.js
+++ server/index.js
@@ -1,16 +1,71 @@
-const http = require('http');
+require('dotenv').config()
+const express = require('express')
+const path = require('path');
+const jokes = require('./controllers/jokes');
+const products = require('./controllers/products')
+const users = require('./controllers/users')
+const excercise = require('./controllers/excercise')
+const workouts = require('./controllers/workouts')
+const { requireLogin, parseAuthorizationHeader } = require('./middleware/authorization')
+const app = express()
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-const server = http.createServer( (req, res) => {
+// Middleware
+app
+    .use(express.json())
+    .use(express.static(path.join(__dirname, '../client/dist')))
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello New Paltz. From Nodemon');
+    // CORS
+    .use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        // Make sure OPTIONS request are always allowed
+        // That way pre-flight requests don't fail
+        if(req.method === 'OPTIONS') {
+            return res.status(200).send({})
+        }
+        next()
+    })
 
-} );
+    .use(parseAuthorizationHeader)
 
-server.listen(port, () => 
-  console.log(`Server running at http://${hostname}:${port}/`)
+
+// Actions
+app
+    .get('/api/v1/', (req, res) => {
+        res.send('Hello World! From Express')
+    })
+    .use('/api/v1/products', requireLogin(), products)
+    .use('/api/v1/users', users)
+    .use('/api/v1/jokes', jokes)
+    .use('/api/v1/excercise', excercise)
+    .use('/api/v1/workouts', workouts)
+
+// Catch all
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
+
+// Error handling
+app
+    .use((err, req, res, next) => {
+        console.error(err);
+        const msg = {
+            status: err.code || 500,
+            error: err.message || 'Internal Server Error',
+            isSuccess: false
+        }
+        res.status(msg.status).json(msg)
+    })
+
+
+console.log('1: About to start server')
+
+app.listen(port, () => 
+  console.log(`2: Server running at http://${hostname}:${port}/`)
 );
+
+console.log('3: Asked server to start')
